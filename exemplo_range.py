27358cc16ba2004653beca02429d6314df0d2f0e
MARCUS PAULO MELO GONZAGA <marcus.gonzaga@aluno.senac.rio>
2025-08-28 21:43:26 -0300

    Estruturas de Repetição e range

---
+++ exemplo_range.py
@@ -0,0 +1,8 @@
+#for i in range(2, 15):
+#    print(i)
+
+#for j in range(5, 41, 5):
+#    print(j)
+
+for z in range(6, 0, -2):
+    print(z)
---
+++ soma.py
@@ -0,0 +1,15 @@
+soma = 0
+#n = range(3)
+#print(n)
+
+for i in range(10): #estrutura de repetição quando eu sei o número de repetições
+#    print(i)
+    n = int(input(f"Informe {i+1}º número: ")) #{i+1} para que o começo seja em 1º não em 0º
+    soma = soma + n
+print(f"A soma é {soma}")
+
+"""n1 = int(input("Informe um número: "))
+n2 = int(input("Informe outro número: "))
+n3 = int(input("Informe mais um número: "))
+soma = n1 + n2 + n3"""
+#print(f"A soma é: {soma}")
